5abed4323a0dc8384e37565158d2bb79f14f01d3
suzuki <c0114292a2@edu.teu.ac.jp>
2014-12-11 18:25:55 +0900

    追加書き込みの実装

--- LineEditer.c
+++ LineEditer.c
@@ -3,18 +3,20 @@
 int main(int argc, char const *argv[])
 {
 	FILE *fp;
-	char str[256];
+	char LineEdite[256];
 	printf("%s\n", argv[1]);
 
-	fp = fopen(argv[1], "w");
-	fputs(argv[1], fp);
+	fp = fopen(argv[1], "a");
 	if(fp==NULL){//ファイルが開けなければ強制終了
 		printf("file can't open\n");
 		return -1;
 	}
-	
-	// printf("%s\n", fgets(fp));
 
+
+	printf(">");
+	scanf("%s", &LineEdite);
+	// printf("%s\n", fgets(fp));
+	fputs(LineEdite, fp);
 	fclose(fp);
 	
 	return 0;
