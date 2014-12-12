3ce4a30bbb64279d3187bf09e29138d7371216a6
suzuki <c0114292a2@edu.teu.ac.jp>
2014-12-12 09:30:38 +0900

    input system change scanf to fgets

--- LineEditer.c
+++ LineEditer.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
+#define WIDTH 80;//コマンドプロンプトの横幅
+
 int main(int argc, char const *argv[])
 {
 	FILE *fp;
-	char LineEdite[256];
-	printf("%s\n", argv[1]);
+	char LineEdite[256];//1行バッファ
+	int tmp[100];
+	printf("filePath: %s\n", argv[1]);//d 読み込んだファイルパスの表示
 
-	fp = fopen(argv[1], "a");
+	fp = fopen(argv[1], "w");
 	if(fp==NULL){//ファイルが開けなければ強制終了
 		printf("file can't open\n");
 		return -1;
 	}
 
-
-	printf(">");
-	scanf("%s", &LineEdite);
-	// printf("%s\n", fgets(fp));
-	fputs(LineEdite, fp);
+	fgets(LineEdite, sizeof(LineEdite), stdin);//fgets(文字配列,sizeof(文字配列),stdin(ファイルポインタ)); ＊stdinはキーボードのファイルポインタ
+	
+	fputs(LineEdite, fp);//fgets(文字配列,ファイルポインタ);
 	fclose(fp);
 	
 	return 0;
