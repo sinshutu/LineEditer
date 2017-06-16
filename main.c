727796e00ee7737ecfcff38e84694b0c5a5131e9
NaotoSuzuki <c0114292a2@edu.teu.ac.jp>
2017-06-16 16:04:03 +0900

    fgetsとputsで書き換え

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-	FILE *fp;
-	char LineEdite[256];
-	printf("%s\n", argv[1]);
+    FILE *fp;
+    char *str;
+    str = (char *)malloc(1000);
+    fp = fopen(argv[1], "a");
+    if(fp==NULL){// ファイルが開けなければ強制終了
+        puts("file can't open\n");
+        return -1;
+    }
 
-	fp = fopen(argv[1], "a");
-	if(fp==NULL){//ファイルが開けなければ強制終了
-		printf("file can't open\n");
-		return -1;
-	}
+    putchar('>');
+    fgets(str, sizeof str, stdin);
+    fputs(str, fp);
+    fclose(fp);
 
-
-	printf(">");
-	scanf("%s", &LineEdite);
-	// printf("%s\n", fgets(fp));
-	fputs(LineEdite, fp);
-	fclose(fp);
-	
-	return 0;
+    return 0;
 }
